a340dae3b6ba68b4569b0e4975f4ac9484959be9
Piyush <bhangalepiyush@gmail.com>
2023-11-26 13:28:20 +0530

    update default layout add navbar

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,8 +1,12 @@
+import Navbar from "../components/Navbar"
 
 export default function DefaultLayout({ children }) {
   return (
     <>
-      <main>{children}</main>
+      <div className="min-h-screen bg-gray-300 text-black font-mono flex-col gap-4">
+        <Navbar />
+        <main>{children}</main>
+      </div>
     </>
   )
 }
