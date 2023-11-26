a1de95f354f3cf8e4aae5ce5a0250fae4c2d5081
Piyush <bhangalepiyush@gmail.com>
2023-11-26 14:45:47 +0530

    feat: landing page on home screen

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -3,9 +3,9 @@ import Navbar from "../components/Navbar"
 export default function DefaultLayout({ children }) {
   return (
     <>
-      <div className="min-h-screen bg-gray-300 text-black font-mono flex-col gap-4">
+      <div className="bg-gray-300 text-black font-mono flex-col gap-4">
         <Navbar />
-        <main>{children}</main>
+        <main className="">{children}</main>
       </div>
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import * as fcl from '@onflow/fcl'
 import useCurrentUser from '../hooks/useCurrentUser'
+import clsx from 'clsx'
 
 export default function Home() {
-  const { loggedIn } = useCurrentUser()
+  const { loggedIn, addr } = useCurrentUser()
+
 
   return (
-    <div>
+    <div className='h-screen  w-full flex items-center justify-center' >
 
       <Head>
         <title>Marketplace</title>
@@ -13,7 +16,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      Hello
+      <div className='flex flex-col gap-4 items-center justify-center'>
+        <h1 className='text-4xl font-bold uppercase'>Flow powered Chat Rooms~</h1>
+
+
+        <button
+          onClick={
+            loggedIn ? fcl.unauthenticate : fcl.authenticate
+          }
+          className={
+            clsx(
+              'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded',
+              {
+                'bg-green-500 hover:bg-green-700': loggedIn,
+                'bg-blue-500 hover:bg-blue-700': !loggedIn,
+              }
+            )
+          }
+        >
+          {
+            !loggedIn ? 'Login' : 'Continue as ' + addr
+          } {"->"}
+        </button>
+      </div>
+
     </div>
   )
 }
