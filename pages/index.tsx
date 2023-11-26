191335196810be091ff91fc502417384cd3b2988
Piyush <bhangalepiyush@gmail.com>
2023-11-26 12:16:31 +0530

    remove boilerplate and add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Links from '../components/Links'
-import Container from '../components/Container'
 import useCurrentUser from '../hooks/useCurrentUser'
 
 export default function Home() {
   const { loggedIn } = useCurrentUser()
 
   return (
-    <div className={styles.container}>
+    <div>
 
       <Head>
-        <title>FCL Next Scaffold</title>
+        <title>Marketplace</title>
         <meta name="description" content="FCL Next Scaffold for the Flow Blockchain" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href="https://developers.flow.com/tools/fcl-js">FCL</a> Next Scaffold
-        </h1>
-
-        <p className={styles.description}>
-          For the Flow Blockchain
-        </p>
-
-        {loggedIn && <Container />}
-
-        <Links />
-
-      </main>
+      Hello
     </div>
   )
 }
